a7f0b4d9b73338726ec98f86f7ff24dd8e2fc5c8
PDempsey03 <dempsey.202@osu.edu>
2025-12-02 21:59:24 -0500

    Add new mouse move condition

---
+++ Mmc.MonoGame.Input/src/Conditions/MouseMoveCondition.cs
@@ -0,0 +1,15 @@
+﻿namespace Mmc.MonoGame.Input.Conditions
+{
+    public class MouseMoveCondition : Condition
+    {
+        public MouseMoveCondition() : base(InputType.None)
+        {
+
+        }
+
+        public override bool IsConditionMet()
+        {
+            return InputHelper.OldMousePosition != InputHelper.NewMousePosition;
+        }
+    }
+}
