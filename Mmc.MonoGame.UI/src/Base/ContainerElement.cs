466ec0882c5e62dcfde552be3c1e97dfb0b04e96
PDempsey03 <dempsey.202@osu.edu>
2025-12-31 10:43:21 -0500

    Refactor to use UIElementExtensions

--- Mmc.MonoGame.UI/src/Base/ContainerElement.cs
+++ Mmc.MonoGame.UI/src/Base/ContainerElement.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Mmc.MonoGame.UI.Models.Primitives;
 
 namespace Mmc.MonoGame.UI.Base
 {
@@ -96,46 +95,11 @@ namespace Mmc.MonoGame.UI.Base
             foreach (var child in Children)
             {
                 // determine how much space the child should be given
-                Rectangle childSlot = CalculateChildRectangle(child, _contentBounds);
+                Rectangle childSlot = child.CalculateChildRectangle(_contentBounds);
 
                 // pass in how much space the child has been given
                 child.Arrange(childSlot);
             }
-        }
-
-        private static Rectangle CalculateChildRectangle(UIElement child, Rectangle contentBounds)
-        {
-            // determine width which for stretch is the whole rectangle's width, otherwise, clamp its desired width to rectangle width
-            float width = child.HorizontalAlignment switch
-            {
-                HorizontalAlignment.Stretch => contentBounds.Width,
-                _ => Math.Min(child.DesiredSize.X, contentBounds.Width)
-            };
-
-            // same for the height as the width
-            float height = child.VerticalAlignment switch
-            {
-                VerticalAlignment.Stretch => contentBounds.Height,
-                _ => Math.Min(child.DesiredSize.Y, contentBounds.Height)
-            };
-
-            // determine x based on the horizontal alignment type
-            float x = child.HorizontalAlignment switch
-            {
-                HorizontalAlignment.Center => contentBounds.X + (contentBounds.Width - width) / 2f,
-                HorizontalAlignment.Right => contentBounds.X + contentBounds.Width - width,
-                _ => contentBounds.X // left and stretch both start at the far left
-            };
-
-            // determine y based on the vertical alignment type
-            float y = child.VerticalAlignment switch
-            {
-                VerticalAlignment.Center => contentBounds.Y + (contentBounds.Height - height) / 2f,
-                VerticalAlignment.Bottom => contentBounds.Y + contentBounds.Height - height,
-                _ => contentBounds.Y // top and stretch both start at the top
-            };
-
-            return new Rectangle((int)x, (int)y, (int)width, (int)height);
         }
     }
 }
