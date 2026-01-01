24906e0173ef1f526440f2ad00527d90a257bf20
PDempsey03 <dempsey.202@osu.edu>
2025-12-31 20:46:38 -0500

    Add thickness operators

--- Mmc.MonoGame.UI/src/Models/Primitives/Thickness.cs
+++ Mmc.MonoGame.UI/src/Models/Primitives/Thickness.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mmc.MonoGame.UI.Models.Primitives
 {
-    public struct Thickness
+    public struct Thickness : IEquatable<Thickness>
     {
         public static readonly Thickness None = new Thickness(0);
 
@@ -30,5 +30,43 @@
         }
 
         public Thickness() : this(0) { }
+
+        public static Thickness operator +(Thickness a, Thickness b)
+        {
+            return new Thickness(a.Left + b.Left, a.Top + b.Top, a.Right + b.Right, a.Bottom + b.Bottom);
+        }
+
+        public static Thickness operator -(Thickness a, Thickness b)
+        {
+            return new Thickness(a.Left - b.Left, a.Top - b.Top, a.Right - b.Right, a.Bottom - b.Bottom);
+        }
+
+        public static bool operator ==(Thickness left, Thickness right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Thickness left, Thickness right)
+        {
+            return !(left == right);
+        }
+
+        public readonly bool Equals(Thickness other)
+        {
+            return Left == other.Left &&
+                   Top == other.Top &&
+                   Right == other.Right &&
+                   Bottom == other.Bottom;
+        }
+
+        public override readonly bool Equals(object? obj)
+        {
+            return obj is Thickness other && Equals(other);
+        }
+
+        public override readonly int GetHashCode()
+        {
+            return HashCode.Combine(Left, Top, Right, Bottom);
+        }
     }
 }
